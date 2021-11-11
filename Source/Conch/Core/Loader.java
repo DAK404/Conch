04abd58659828e20b7e19ff477ddd58ecf0220af
DAK <46497387+DAK404@users.noreply.github.com>
2021-11-11 11:44:06 +0530

    Improved Loader.java

--- Source/Conch/Core/Loader.java
+++ Source/Conch/Core/Loader.java
@@ -1,7 +1,13 @@
 package Conch.Core;
 
+import java.io.Console;
+import java.io.File;
+
 public class Loader 
 {
+    Console console = System.console();
+
+    String status = "OK";
     public void loadLogic()
     {
         debugLoad();
@@ -10,15 +16,43 @@ public class Loader
     private void debugLoad()
     {
         System.out.println("Conch Booted successfully.");
+        checkFiles();
+        System.out.println("Checking Files... " + status);
+        while(loaderMenu());
     }
     
     private boolean checkFiles()
     {
-        return true;
+        boolean result = false;
+
+        String[] dirs = {"./Users", "./System", "./System/Private", "./System/Public"};
+
+        for (String string : dirs) 
+        {
+            if (! new File(string).exists())
+            {
+                status = "FAILED";
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     private boolean loaderMenu()
     {
+        String input = console.readLine("~DBG@CONCH: ");
+        switch(input)
+        {
+            case "exit":
+                return false;
+
+            case "":
+                break;
+
+            default:
+                System.out.println("COMMAND NOT RECOGNIZED");
+        }
         return true;
     }
 }
