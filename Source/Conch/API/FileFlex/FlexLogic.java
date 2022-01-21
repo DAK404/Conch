5b2c67c891872d93c6358d81b9bacc584789d766
DAK <46497387+DAK404@users.noreply.github.com>
2022-01-21 10:09:10 +0530

    Update FlexLogic.java

--- Source/Conch/API/FileFlex/FlexLogic.java
+++ Source/Conch/API/FileFlex/FlexLogic.java
@@ -11,5 +11,77 @@ import java.io.OutputStream;
 
 public class FlexLogic 
 {
+    private final String ARGUMENT_MISMATCH = """
+    [E] : Invalid argument length.
+    """;
+    private final String ARGUMENT_PATH_INVALID = """
+    [A] : The source/destination path/file does not exist.
+    """;
+    private final String DEST_FILE_EXISTS = """
+    [A] : A file with the same name exists in the destination path.
+    """;
+
     private Console console = System.console();
+
+    public void fileFlex()throws Exception
+    {
+        String temp;
+        do
+        {
+            temp = console.readLine();
+            fileFlexLogic(temp);
+        }while(!temp.equalsIgnoreCase("exit"));
+    }
+
+    private void fileFlexLogic(String input)throws Exception
+    {
+        String[] cmd = input.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+        switch(cmd[0])
+        {
+            case "cp":
+            case "copy":
+                System.out.println((cmd.length)<3?ARGUMENT_MISMATCH + "Syntax: copy <source> <destination>":"Copying File...");
+                break;
+            
+            case "mv":
+            case "move":
+                System.out.println((cmd.length)<3?ARGUMENT_MISMATCH + "Syntax: move <source> <destination>":"Moving File...");
+                break;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////
+    // LOGIC TO NAVIGATE THE USER HOME DIRECTORY
+    ///////////////////////////////////////////////////////////////
+
+    private void changeDir(String fileName)throws Exception
+    {
+
+    }
+
+    private void resetToHomeDir()throws Exception
+    {
+
+    }
+
+    private void prevDir()throws Exception
+    {
+
+    }
+
+    ///////////////////////////////////////////////////////////////
+    // LOGIC TO CHECK FOR A FILE OR DIRECTORY
+    ///////////////////////////////////////////////////////////////
+
+    private final boolean checkFile(String fileName)throws Exception
+    {
+        return new File(fileName).exists();
+    }
+
+    ///////////////////////////////////////////////////////////////
+    // LOGIC TO PRINT THE DIRECTORY IN A TREE FORMAT
+    ///////////////////////////////////////////////////////////////
+
+    
+
 }
