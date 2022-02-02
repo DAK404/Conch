08539ed6272910c8b1bceb1804dac85d586af503
DAK <46497387+DAK404@users.noreply.github.com>
2022-02-02 10:54:47 +0530

    Update FlexLogic.java

--- Source/Conch/API/FileFlex/FlexLogic.java
+++ Source/Conch/API/FileFlex/FlexLogic.java
@@ -1,6 +1,5 @@
 package Conch.API.FileFlex;
 
-import java.io.BufferedReader;
 import java.io.Console;
 import java.io.File;
 import java.io.FileReader;
@@ -8,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import Conch.API.PrintStreams;
 
 public class FlexLogic 
 {
@@ -20,6 +21,21 @@ public class FlexLogic
     private final String DEST_FILE_EXISTS = """
     [A] : A file with the same name exists in the destination path.
     """;
+
+    private final String SOURCE_DEST_SAME = """
+    [A] : The source and destination paths are the same.
+    """;
+
+    private final String INVALID_COMMAND = """
+    [E] : COMMMAND NOT FOUND.
+    """;
+
+    // private final String <reserved for future use> = """
+    // """;
+
+    private String _username = "";
+    private String _name = "";
+    private String _currentDirectory = "";
 
     private Console console = System.console();
 
@@ -46,6 +62,10 @@ public class FlexLogic
             case "mv":
             case "move":
                 System.out.println((cmd.length)<3?ARGUMENT_MISMATCH + "Syntax: move <source> <destination>":"Moving File...");
+                break;
+
+            default:
+                System.out.println(INVALID_COMMAND);
                 break;
         }
     }
@@ -82,6 +102,104 @@ public class FlexLogic
     // LOGIC TO PRINT THE DIRECTORY IN A TREE FORMAT
     ///////////////////////////////////////////////////////////////
 
-    
+    private final void treeViewer()throws Exception
+    {
+        try
+        {
+            File tree=new File(_currentDirectory);
+            System.out.println("\n--- [ TREE VIEW ] ---\n");
+            treeViewerLogic(0, tree);
+            System.out.println();
+            System.gc();
+        }
+        catch(Exception E)
+        {
+            E.printStackTrace();
+        }
+    }
 
+    private final void treeViewerLogic(int indent, File fileName)throws Exception
+    {
+        try
+        {
+            System.out.print('|');
+
+            for (int i = 0; i < indent; ++i)
+                System.out.print('=');
+
+            System.out.println(fileName.getName().replace(_username, _name + " [ USER ROOT DIRECTORY ]"));
+
+            if (fileName.isDirectory())
+            {
+                File[] files = fileName.listFiles();
+
+                for (int i = 0; i < files.length; ++i)
+                treeViewerLogic(indent + 2, files[i]);
+            }
+        }
+        catch(Exception e)
+        {
+            PrintStreams.printError(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////
+    // LOGIC TO LIST ALL THE FILES IN A DIRECTORY
+    ///////////////////////////////////////////////////////////////
+
+    public void listFiles()throws Exception
+    {
+        String format = "%1$-32s| %2$-24s| %3$-10s\n";
+        String c = "-";
+        if(checkFile(_currentDirectory))
+        {
+            File dPath=new File(_currentDirectory);
+            System.out.println("\n");
+            String disp = (String.format(format, "Directory/File Name", "File Size [In KB]","Type"));
+            System.out.println(disp + c.repeat(disp.length()) + "\n");
+            for(File file : dPath.listFiles())
+            {
+                //System.out.format(String.format(format, file.getPath().replace(User,Name), file.getName().replace(User,Name), file.length()/1024+" KB"));
+                System.out.format(String.format(format, file.getName().replace(_username, _name), file.length()/1024+" KB", file.isDirectory()?"Directory":"File"));
+            }
+            System.out.println();
+        }
+        else
+        System.out.println("[ ERROR ] : The specified file/directory does not exist.");
+        System.gc();
+    }
+
+    ///////////////////////////////////////////////////////////////
+    // LOGIC TO COPY/MOVE A FILE OR DIRECTORY
+    ///////////////////////////////////////////////////////////////
+
+    private final void copyMoveFile(boolean move, String sourceFile, String destinationFile)throws Exception
+    {
+        try
+        {
+            if(checkFile(sourceFile))
+            {
+                if(sourceFile.equalsIgnoreCase(destinationFile))
+                {
+                    System.out.println();
+
+                }
+                else
+                {
+                    if(checkFile(destinationFile))
+                        System.out.println(DEST_FILE_EXISTS);
+                }
+            }
+            else
+            {
+
+            }
+
+        }
+        catch(Exception E)
+        {
+
+        }
+    }
 }
